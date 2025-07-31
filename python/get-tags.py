c004c23e96226d9f9f028cd1ba5009d7daf297d0
Laura Untner <laura.untner@reflex.at>
2025-07-31 17:23:22 +0200

    ignore status

--- python/get-tags.py
+++ python/get-tags.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 import csv
 from collections import defaultdict
 
-IGNORE_TAGS = {"XML", "title", "text"}
+IGNORE_TAGS = {"XML", "title", "text", "status"}
 
 def remove_namespace(tag):
     return tag.split('}', 1)[1] if '}' in tag else tag
